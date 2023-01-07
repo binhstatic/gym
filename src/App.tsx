7603d27ce6ba73fd16b61ea6f6d1d4797de99224
Van Binh <nguyenvanbinh13112000@outlook.com>
2023-01-07 15:55:35 +0700

    feature: Navbar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+  return (
+    <div className="app bg-gray-20 ">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,9 +14,9 @@ module.exports = {
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
-    },backgroundImage: (theme) => ({
-      "gradient-yellowred":
-        "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+    },
+    backgroundImage: (theme) => ({
+      "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
       "mobile-home": "url('./assets/HomePageGraphic.png')",
     }),
     fontFamily: {
