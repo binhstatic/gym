bffc1e2745428088e92a127a09a8fe40e638c1df
Van Binh <nguyenvanbinh13112000@outlook.com>
2023-01-31 07:56:12 +0700

    feature: Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
+import Home from "./scenes/home";
 import { SelectedPage } from "./shared/types";
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
